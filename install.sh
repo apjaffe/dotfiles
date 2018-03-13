cdf1cd195855145ec92f16e6f0c1c21c8818c9de
alan <alan>
2018-03-13 21:15:46 +0000

    Fix tmux conf

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.tmuxrc ~/.tmuxrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 echo "if [ -f ~/dotfiles/bashrc ]; then . ~/dotfiles/bashrc; fi" >> ~/.bashrc
 
